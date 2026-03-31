c6e268c149a424a9a3d0adf1d3dcb34570982235
rude-jh <3372447187@qq.com>
2026-03-31 20:01:35 +0800

    up

--- train.py
+++ train.py
@@ -1,9 +1,20 @@
 from ultralytics import YOLO
 
+# if __name__ == '__main__':
+#     model = YOLO("yolov8n.pt")
+#     model.train(data=r"D:\yolo\code\mydata.yaml",
+#                 epochs=150,
+#                 batch=8,
+#                 workers=0,
+#                 imgsz=640)
+
+
 if __name__ == '__main__':
     model = YOLO("yolov8n.pt")
-    model.train(data=r"D:\yolo\code\mydata.yaml",
-                epochs=150,
-                batch=8,
-                workers=0,
-                imgsz=640)
+    model.train(
+        data="data/data.yaml",
+        epochs=150,
+        batch=52,
+        workers=0,
+        imgsz=640
+    )
--- val.py
+++ val.py
@@ -1,8 +1,20 @@
 from ultralytics import YOLO
 
+# 原版代码
+# if __name__ == '__main__':
+#     model = YOLO("./runs/detect/train/weights/best.pt")
+#     model.val(data=r"D:\yolo\code\mydata.yaml",
+#               batch=8,
+#               workers=0,
+#               imgsz=640)
+
+from ultralytics import YOLO
+
 if __name__ == '__main__':
     model = YOLO("./runs/detect/train/weights/best.pt")
-    model.val(data=r"D:\yolo\code\mydata.yaml",
-              batch=8,
-              workers=0,
-              imgsz=640)
+    model.val(
+        data="data/data.yaml",
+        batch=8,
+        workers=0,
+        imgsz=640
+    )
